781e622453c89c575b7e5f70ee1443112305f466
imrane ALI LAFKIH <imrane.laf@gmail.com>
2025-01-17 21:37:51 +0100

    feat: add upload page UI and progress component (not functional well yet

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,12 +1,12 @@
 function Footer() {
   return (
-    <div className="w-full h-fit lg:flex p-8 lg:px-14 bg-slate-50 text-gray-700 italic lg:justify-between border-t text-center">
-        <div>
+    <div className="w-full h-fit lg:flex p-4 lg:px-14 bg-slate-50 text-gray-700 italic lg:justify-between border-t text-center">
+        <div className="flex lg:mx-7 m-4 justify-center">
         ©   {new Date().getFullYear()} All rights reserved
         </div>
         <ul className="flex mx-5 underline justify-center">
-            <li className="lg:mx-7 m-4">Terms</li>
-            <li className="lg:mx-7 m-4">Privacy Policy</li>
+            <li className="lg:mx-6 m-4">Terms</li>
+            <li className="lg:mx-6 m-4">Privacy Policy</li>
         </ul>
         
     </div>
--- src/app/configure/upload/page.tsx
+++ src/app/configure/upload/page.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+'use client'
 
-function page() {
-  return (
-    <div>page</div>
-  )
+import { Progress } from '@/components/ui/progress'
+import { cn } from '@/lib/utils'
+import { Image, LoaderPinwheel } from 'lucide-react'
+import { useState } from 'react'
+import Dropzone, { FileRejection } from 'react-dropzone'
+
+
+
+function Page() {
+    const [onDrag, setOnDrag] = useState<boolean>(false)
+    const [progress, setProgress] = useState<number>(0)
+    const dropAccepted = () => {
+        console.log("file accepted");
+        setOnDrag(false)
+    }
+    const dropRejected = () => { }
+
+    const isUpload = true
+
+    return (
+        <div className={"w-full p-5 min-h-[calc(100vh_-_9.5rem)] flex justify-center"}>
+
+            <Dropzone
+                onDropAccepted={dropAccepted}
+                onDropRejected={dropRejected}
+                accept={{ "image/png": [".png"], "image/jpeg": [".jpeg"], "image/jpg": [".jpg"] }}
+                onDragEnter={() => setOnDrag(true)}
+                onDragLeave={() => setOnDrag(false)}
+            >
+
+                {
+                    ({ getRootProps, getInputProps }) => (
+                        <div className={cn("relative w-10/12 h-10/12 text-gray-600 bg-slate-100 border border-gray-300 rounded-2xl flex flex-col items-center justify-center",
+                            {
+                                'bg-gray-200 border-gray-600': onDrag
+                            }
+                        )}
+                            {...getRootProps()}
+                        >
+                            <input {...getInputProps()} />
+                            {
+                                isUpload ? (<div className='flex flex-col items-center'>
+                                    <LoaderPinwheel className='animate-spin m-2' />
+                                    <p className='text-gray-600'>Uploading...</p>
+                                     <Progress value={progress} className='mt-2 w-40' /></div>) : 
+                                     (<div className='flex flex-col items-center'>
+                                        <Image  className={cn('m-2', {'animate-bounce': onDrag})}/>
+                                        <p> <span className='font-semibold'>Click to upload</span> or drag and drop</p>
+                                        <p>PNG, JPG, JPEG</p>
+                                     </div>)
+                            }
+                            
+                            
+                        </div>
+                    )
+                }
+
+            </Dropzone>
+        </div>
+
+
+
+    );
 }
 
-export default page
+export default Page;
